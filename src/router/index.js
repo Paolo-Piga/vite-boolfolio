aa5ab6ac1244a24f5e0449e5dcb4557f4d218a32
DecimTheJudge <paolo1piga@gmail.com>
2024-10-02 14:42:48 +0200

    finito

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProgettiView from '../views/ProgettiView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/',
+    name: 'progetti',
+    component: ProgettiView
   },
   {
     path: '/about',
---
+++ src/views/ProgettiView.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <h1>Progetti</h1>
+      <ul>
+        <li v-for="progetto in progettiView" :key="progetto.id">
+          {{ progetto.name }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ProgettiView',
+    data() {
+      return {
+        progettiView: []
+      };
+    },
+    created() {
+      // Chiamata API per i progetti
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => {
+          // Simuliamo che ogni progetto abbia un id e un nome
+          this.progetti = data.map(item => ({
+            id: item.id,
+            name: item.title
+          }));
+        })
+        .catch(error => {
+          console.error("Errore durante il recupero dei progetti: ", error);
+        });
+    }
+  };
+  </script>
